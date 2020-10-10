6fdc2034bc731a6778cc1e9dfa507b977093e316
e <89531004@qq.com>
2020-10-11 06:13:13 +0800

    final

--- librian/librian本體/前端/虛擬核心.js
+++ librian/librian本體/前端/虛擬核心.js
@@ -35,7 +35,7 @@ window.虛擬核心 = {
       "cg": null,
       "視頻": null,
       "js": null,
-      "html": null,
+      "html": "<style>\n[選項號]{\n    width: 20% !important;\n    height: 20% !important;\n    display: inline-block !important;\n    background-size: cover !important;\n    text-shadow: 0px 0px 5px #000 !important;\n    box-shadow: 0px 0px 20px #000 !important;\n}\n[選項號=\"0\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/plane.jpg\") !important;\n}\n[選項號=\"1\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/南极.jpeg\") !important;\n}\n[選項號=\"2\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/雪山.jpg\") !important;\n}\n[選項號=\"3\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/星空.jpg\") !important;\n}\n[選項號=\"4\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/6k.jpg\") !important;\n}\n[選項號=\"5\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/mountain.jpg\") !important;\n}\n[選項號=\"6\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/绝顶.jpg\") !important;\n}\n[選項號=\"7\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/修叽.jpg\") !important;\n}\n[選項號=\"8\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/和風縁側.jpg\") !important;\n}\n</style>",
       "選項": [],
       "特效表": {},
       "額外信息": [],
@@ -61,6 +61,12 @@ window.虛擬核心 = {
             }
           ],
           "類型": "函數調用"
+        },
+        {
+          "縮進數": 0,
+          "代碼類型": "html",
+          "代碼內容": "<style>\n[選項號]{\n    width: 20% !important;\n    height: 20% !important;\n    display: inline-block !important;\n    background-size: cover !important;\n    text-shadow: 0px 0px 5px #000 !important;\n    box-shadow: 0px 0px 20px #000 !important;\n}\n[選項號=\"0\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/plane.jpg\") !important;\n}\n[選項號=\"1\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/南极.jpeg\") !important;\n}\n[選項號=\"2\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/雪山.jpg\") !important;\n}\n[選項號=\"3\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/星空.jpg\") !important;\n}\n[選項號=\"4\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/6k.jpg\") !important;\n}\n[選項號=\"5\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/mountain.jpg\") !important;\n}\n[選項號=\"6\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/绝顶.jpg\") !important;\n}\n[選項號=\"7\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/修叽.jpg\") !important;\n}\n[選項號=\"8\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/和風縁側.jpg\") !important;\n}\n</style>",
+          "類型": "插入代碼"
         },
         {
           "縮進數": 0,
@@ -90,25 +96,81 @@ window.虛擬核心 = {
       "cg": null,
       "視頻": null,
       "js": null,
-      "html": null,
+      "html": "<style>\n[選項號]{\n    width: 20% !important;\n    height: 20% !important;\n    display: inline-block !important;\n    background-size: cover !important;\n    text-shadow: 0px 0px 5px #000 !important;\n    box-shadow: 0px 0px 20px #000 !important;\n}\n[選項號=\"0\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/plane.jpg\") !important;\n}\n[選項號=\"1\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/南极.jpeg\") !important;\n}\n[選項號=\"2\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/雪山.jpg\") !important;\n}\n[選項號=\"3\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/星空.jpg\") !important;\n}\n[選項號=\"4\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/6k.jpg\") !important;\n}\n[選項號=\"5\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/mountain.jpg\") !important;\n}\n[選項號=\"6\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/绝顶.jpg\") !important;\n}\n[選項號=\"7\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/修叽.jpg\") !important;\n}\n[選項號=\"8\"]{\n    background-image: url(\"../project/狂气之峰1/我的圖片/和風縁側.jpg\") !important;\n}\n</style>",
       "選項": [
-        "车卡&登山前的准备",
-        "来到南极开始爬山"
+        "登山前的准备",
+        "来到南极",
+        "修格斯山棱",
+        "彩虹谷",
+        "来到8k",
+        "登顶前的准备",
+        "登顶！",
+        "逃亡",
+        "后日谈"
       ],
       "特效表": {},
       "額外信息": [],
       "源": [
         {
           "縮進數": 0,
-          "選項名": "车卡&登山前的准备",
+          "選項名": "登山前的准备",
           "文件": "登山前.liber",
           "位置": null,
           "類型": "選項"
         },
         {
           "縮進數": 0,
-          "選項名": "来到南极开始爬山",
+          "選項名": "来到南极",
           "文件": "爬山开始.liber",
+          "位置": null,
+          "類型": "選項"
+        },
+        {
+          "縮進數": 0,
+          "選項名": "修格斯山棱",
+          "文件": "修格斯山棱.liber",
+          "位置": null,
+          "類型": "選項"
+        },
+        {
+          "縮進數": 0,
+          "選項名": "彩虹谷",
+          "文件": "狗斗.liber",
+          "位置": null,
+          "類型": "選項"
+        },
+        {
+          "縮進數": 0,
+          "選項名": "来到8k",
+          "文件": "八千.liber",
+          "位置": null,
+          "類型": "選項"
+        },
+        {
+          "縮進數": 0,
+          "選項名": "登顶前的准备",
+          "文件": "登顶前的准备.liber",
+          "位置": null,
+          "類型": "選項"
+        },
+        {
+          "縮進數": 0,
+          "選項名": "登顶！",
+          "文件": "登顶.liber",
+          "位置": null,
+          "類型": "選項"
+        },
+        {
+          "縮進數": 0,
+          "選項名": "逃亡",
+          "文件": "逃亡.liber",
+          "位置": null,
+          "類型": "選項"
+        },
+        {
+          "縮進數": 0,
+          "選項名": "后日谈",
+          "文件": "后日谈.liber",
           "位置": null,
           "類型": "選項"
         }
